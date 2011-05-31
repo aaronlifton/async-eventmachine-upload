814bb61f14594468ef56f7a3eb694c36e4c5ef2d
Aaron Lifton <aaron@Aaron-Ls-MacBook-Pro.local>
2011-05-31 14:05:02 -0400

    erubis

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require 'erubis'
 
 get '/' do
-  "Hello World!"
+  eruby = Erubis::Eruby.new(File.read('home.rhtml'))
+  items = ['foo', 'bar', 'baz']
+  return eruby.evaluate(:items => items)
 end
