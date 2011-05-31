e225957d6d71fedbcbbf8299c3eac13e49c34bba
Aaron Lifton <aaron@Aaron-Ls-MacBook-Pro.local>
2011-05-30 21:00:46 -0400

    sinatra

--- app.rb
+++ app.rb
@@ -1,22 +1,5 @@
-require 'rubygems'
-require erubis
-require sinatra
-require json
-require rest_client
-require active_support
-require sinatra/common_helper
-require middleware
-
-set :sessions, true
-set :show_exceptions, false
-use Rack::Flash
-use Middleware::App
+require 'sinatra'
 
 get '/' do
-  eruby = Erubis::Eruby.new(File.read('home.rhtml'))
-  items = ['foo', 'bar', 'baz']
-  res.body = eruby.evaluate(:items => items)
+  "Hello World!"
 end
-
-
-error do  redirect '/' end
